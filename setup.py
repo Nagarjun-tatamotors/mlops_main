a807ea6d2c9bc6ec75362e4a2f88600a6f2796a7
Nagarjuna Jali <nagarjuna.j@tataelxsi.co.in>
2023-06-27 07:42:51 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ts a wafer project using mlops for learning purposes',
+    author='nagarjun',
+    license='MIT',
+)
